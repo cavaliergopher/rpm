c217da3a11eee56f317532edd7a077847df3f4dc
Ryan Armstrong <ryan@cavaliercoder.com>
2016-01-04 01:04:36 +0800

    Added MD5Check() checksum validation

---
+++ example_md5check_test.go
@@ -0,0 +1,26 @@
+package rpm
+
+import (
+	"fmt"
+	"github.com/cavaliercoder/go-rpm"
+	"os"
+)
+
+func ExampleMD5Check() {
+	// open a rpm package for reading
+	f, err := os.Open("my-package.rpm")
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	// validate md5 checksum
+	if err := rpm.MD5Check(f); err == nil {
+		fmt.Printf("Package passed checksum validation\n")
+	} else if err == rpm.ErrMD5ValidationFailed {
+		fmt.Printf("Package failed checksum validation\n")
+	} else {
+		panic(err)
+	}
+}
---
+++ gpgcheck.go
@@ -0,0 +1,72 @@
+package rpm
+
+import (
+	"bytes"
+	"crypto/md5"
+	"fmt"
+	"io"
+)
+
+var (
+	// ErrMD5ValidationFailed indicates that a RPM package failed checksum
+	// validation.
+	ErrMD5ValidationFailed = fmt.Errorf("Package checksum validation failed")
+)
+
+// MD5Check validates the integrity of a RPM package file read from the given
+// io.Reader. An MD5 checksum is computed for the package payload and compared
+// with the checksum value specified in the package header.
+//
+// If validation succeeds, nil is returned. If validation fails, a GPGCheckError
+// is returned.
+//
+// This function is an expensive operation which reads the entire package file.
+func MD5Check(r io.Reader) error {
+	_, _, err := md5check(r)
+	return err
+}
+
+// md5check reads a RPM package file from the given io.Reader and returns the
+// signature header and computed md5 sum for the package payload.
+//
+// If the package fails checksum validation, a GPGCheckError is returned.
+func md5check(r io.Reader) (*Header, []byte, error) {
+	// read package lead
+	if _, err := ReadPackageLead(r); err != nil {
+		return nil, nil, err
+	}
+
+	// read signature header
+	sigheader, err := ReadPackageHeader(r)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// get expected payload size
+	payloadSize := sigheader.Indexes.IntByTag(1000)
+	if payloadSize == 0 {
+		return nil, nil, fmt.Errorf("No payload size specified")
+	}
+
+	// get expected payload md5 sum
+	sigmd5 := sigheader.Indexes.BytesByTag(1004)
+	if sigmd5 == nil {
+		return nil, nil, fmt.Errorf("No signature md5 sum found")
+	}
+
+	// compute payload sum
+	h := md5.New()
+	if n, err := io.Copy(h, r); err != nil {
+		return nil, nil, fmt.Errorf("Error reading payload: %v", err)
+	} else if n != payloadSize {
+		return nil, nil, ErrMD5ValidationFailed
+	}
+
+	// compare sums
+	payloadmd5 := h.Sum(nil)
+	if !bytes.Equal(payloadmd5, sigmd5) {
+		return nil, nil, ErrMD5ValidationFailed
+	}
+
+	return sigheader, h.Sum(nil), nil
+}
