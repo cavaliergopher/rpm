4ebacd4ecc1f350380ecc097cb6ff73829613858
Ryan Armstrong <ryan@cavaliercoder.com>
2016-01-05 00:30:05 +0800

    Added keyring helper functions

--- example_gpgcheck_test.go
+++ example_gpgcheck_test.go
@@ -3,44 +3,28 @@ package rpm_test
 import (
 	"fmt"
 	"github.com/cavaliercoder/go-rpm"
-	"golang.org/x/crypto/openpgp"
-	"golang.org/x/crypto/openpgp/armor"
 	"os"
 )
 
 // ExampleGPGCheck reads a public GPG key and uses it to validate the signature
 // of a local rpm package.
 func ExampleGPGCheck() {
-	// open gpgkey file (typically in /etc/pki/rpm-gpg)
-	f_gpgkey, err := os.Open("fixtures/RPM-GPG-KEY-CentOS-7")
-	if err != nil {
-		panic(err)
-	}
-
-	defer f_gpgkey.Close()
-
-	// decode gpgkey file
-	p, err := armor.Decode(f_gpgkey)
-	if err != nil {
-		panic(err)
-	}
-
-	// read public key from decoded gpgkey file
-	keyring, err := openpgp.ReadKeyRing(p.Body)
+	// read public key from gpgkey file
+	keyring, err := rpm.KeyRingFromFile("fixtures/RPM-GPG-KEY-CentOS-7")
 	if err != nil {
 		panic(err)
 	}
 
 	// open a rpm package for reading
-	f_rpm, err := os.Open("fixtures/centos-release-7-2.1511.el7.centos.2.10.x86_64.rpm")
+	f, err := os.Open("fixtures/centos-release-7-2.1511.el7.centos.2.10.x86_64.rpm")
 	if err != nil {
 		panic(err)
 	}
 
-	defer f_rpm.Close()
+	defer f.Close()
 
 	// validate gpg signature
-	if signer, err := rpm.GPGCheck(f_rpm, keyring); err == nil {
+	if signer, err := rpm.GPGCheck(f, keyring); err == nil {
 		fmt.Printf("Package signed by '%s'\n", signer)
 	} else if err == rpm.ErrGPGValidationFailed {
 		fmt.Printf("Package failed GPG signature validation\n")
--- gpgcheck_test.go
+++ gpgcheck_test.go
@@ -1,56 +1,12 @@
 package rpm
 
 import (
-	"fmt"
-	"golang.org/x/crypto/openpgp"
-	"golang.org/x/crypto/openpgp/armor"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
 )
-
-// keyring loads all gpgkey files in the fixtures directory and returned a
-// keyring containing all found entities.
-func keyring() (openpgp.KeyRing, error) {
-	// read fixtures directory
-	dir, err := ioutil.ReadDir("fixtures")
-	if err != nil {
-		return nil, fmt.Errorf("Error reading fixtures directory: %v", err)
-	}
-
-	keyring := make(openpgp.EntityList, 0)
-	for _, fi := range dir {
-		if strings.HasPrefix(fi.Name(), "RPM-GPG-KEY-") {
-			// open gpgkey file
-			f, err := os.Open(filepath.Join("fixtures", fi.Name()))
-			if err != nil {
-				return nil, fmt.Errorf("Error reading %s: %v", fi.Name(), err)
-			}
-			defer f.Close()
-
-			// decode gpgkey file
-			p, err := armor.Decode(f)
-			if err != nil {
-				return nil, fmt.Errorf("Error decoding %s: %v", fi.Name(), err)
-			}
-
-			// extract keys
-			el, err := openpgp.ReadKeyRing(p.Body)
-			if err != nil {
-				return nil, fmt.Errorf("Error reading keyring in %s: %v", fi.Name(), err)
-			}
-
-			// append to keyring
-			for _, e := range el {
-				keyring = append(keyring, e)
-			}
-		}
-	}
-
-	return keyring, nil
-}
 
 func TestMD5Check(t *testing.T) {
 	// load package file paths
@@ -77,16 +33,30 @@ func TestMD5Check(t *testing.T) {
 }
 
 func TestGPGCheck(t *testing.T) {
+	// read fixtures directory
+	dir, err := ioutil.ReadDir("fixtures")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	// filter for gpgkey files
+	keyfiles := make([]string, 0)
+	for _, fi := range dir {
+		if strings.HasPrefix(fi.Name(), "RPM-GPG-KEY-") {
+			keyfiles = append(keyfiles, filepath.Join("fixtures", fi.Name()))
+		}
+	}
+
+	// build keyring
+	keyring, err := KeyRingFromFiles(keyfiles)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
 	// load package file paths
 	files, err := packages(t)
 	if err != nil {
 		t.Fatalf("Error listing rpm packages: %v", err)
-	}
-
-	// load keys
-	keyring, err := keyring()
-	if err != nil {
-		t.Fatalf(err.Error())
 	}
 
 	// check each package
@@ -106,5 +76,4 @@ func TestGPGCheck(t *testing.T) {
 	}
 
 	t.Logf("Validated GPG signature for %d packages", valid)
-
 }
