080385835708629a3c095eaa579b861aa79920d8
Ryan Armstrong <ryan@cavaliercoder.com>
2016-01-04 17:18:51 +0800

    Added rpm files to test fixtures

--- packagefile_test.go
+++ packagefile_test.go
@@ -9,35 +9,49 @@ import (
 )
 
 func TestReadRPMFile(t *testing.T) {
-	// get a directory full of rpms from RPM_DIR environment variable but allow
-	// tests to suceed if no rpms are available (like on Travis CI).
+	// get a directory full of rpms from RPM_DIR environment variable or
+	// failback to ./fixtures
 	dir := os.Getenv("RPM_DIR")
-	if dir != "" {
+	if dir == "" {
+		dir = "fixtures"
+	}
 
-		// list RPM files
-		files, err := ioutil.ReadDir(dir)
-		if err != nil {
-			t.Fatalf(err.Error())
-		}
+	// list RPM files
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
 
-		valid := 0
-		for _, f := range files {
-			if strings.HasSuffix(f.Name(), ".rpm") {
-				path := filepath.Join(dir, f.Name())
-				rpm, err := OpenPackageFile(path)
-				if err != nil {
-					t.Errorf("Error loading RPM file %s: %s", f.Name(), err)
-				} else {
-					t.Logf("Loaded package: %v", rpm)
-					valid++
-				}
+	valid := 0
+	for _, f := range files {
+		if strings.HasSuffix(f.Name(), ".rpm") {
+			path := filepath.Join(dir, f.Name())
+
+			// MD5 Check
+			f, _ := os.Open(path)
+			defer f.Close()
+
+			if err := MD5Check(f); err != nil {
+				t.Errorf("Validation error for %s: %v", f.Name(), err)
+				//os.Remove(path)
+			}
+			f.Close()
+
+			// Load package info
+			rpm, err := OpenPackageFile(path)
+			if err != nil {
+				t.Errorf("Error loading RPM file %s: %s", f.Name(), err)
+				//os.Remove(path)
+			} else {
+				t.Logf("Loaded package: %v", rpm)
+				valid++
 			}
 		}
+	}
 
-		if valid == 0 {
-			t.Errorf("No RPM files found for testing with in %s", dir)
-		} else {
-			t.Logf("Validated %d RPM files", valid)
-		}
+	if valid == 0 {
+		t.Errorf("No RPM files found for testing with in %s", dir)
+	} else {
+		t.Logf("Validated %d RPM files", valid)
 	}
 }
