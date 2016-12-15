0d4d61208565c33ff44f640ffdb3a02dee284dd3
Ryan Armstrong <ryan@cavaliercoder.com>
2016-12-15 22:34:44 +0800

    Removed misleading Package interface

--- package.go
+++ package.go
@@ -1,9 +1,5 @@
 package rpm
 
-import (
-	"time"
-)
-
 // PackageVersion is an interface which holds version information for a single
 // package version.
 type PackageVersion interface {
@@ -12,45 +8,3 @@ type PackageVersion interface {
 	Release() string
 	Epoch() int
 }
-
-// Package is an interface which represents an RPM package and its supported
-// tags.
-type Package interface {
-	PackageVersion
-
-	Architecture() string
-
-	Path() string
-	FileTime() time.Time
-	FileSize() uint64
-
-	Size() uint64
-	ArchiveSize() uint64
-
-	BuildTime() time.Time
-
-	HeaderStart() uint64
-	HeaderEnd() uint64
-
-	Summary() string
-	Description() string
-	URL() string
-
-	License() string
-	Vendor() string
-	Groups() []string
-	BuildHost() string
-	SourceRPM() string
-	Packager() string
-
-	Requires() Dependencies
-	Conflicts() Dependencies
-	Obsoletes() Dependencies
-	Provides() Dependencies
-
-	Checksum() (string, error)
-	ChecksumType() string
-}
-
-// Packages is a slice of Package interfaces.
-type Packages []Package
