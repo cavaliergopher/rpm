9664735b838ea0a81e4aace3197ebe0d4040f952
Ryan Armstrong <ryan@cavaliercoder.com>
2017-01-16 14:33:03 +0800

    Fixed timezone in PackageFile.Files example

--- example_packagefile_files_test.go
+++ example_packagefile_files_test.go
@@ -5,7 +5,7 @@ import (
 	"github.com/cavaliercoder/go-rpm"
 )
 
-// ExamplePackageFile_Files lists all the files in a RPM package.
+// Lists all the files in a RPM package.
 func ExamplePackageFile_Files() {
 	// open a package file
 	pkg, err := rpm.OpenPackageFile("./testdata/epel-release-7-5.noarch.rpm")
@@ -22,17 +22,17 @@ func ExamplePackageFile_Files() {
 			fi.Owner(),
 			fi.Group(),
 			fi.Size(),
-			fi.ModTime().Format("Jan 02 15:04"),
+			fi.ModTime().UTC().Format("Jan 02 15:04"),
 			fi.Name())
 	}
 
 	// Output:
 	// total 7
-	// -rw-r--r-- root root  1662 Nov 26 00:23 /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
-	// -rw-r--r-- root root  1056 Nov 26 00:23 /etc/yum.repos.d/epel-testing.repo
-	// -rw-r--r-- root root   957 Nov 26 00:23 /etc/yum.repos.d/epel.repo
-	// -rw-r--r-- root root    41 Nov 26 00:23 /usr/lib/rpm/macros.d/macros.epel
-	// -rw-r--r-- root root  2813 Nov 26 00:23 /usr/lib/systemd/system-preset/90-epel.preset
-	// -rwxr-xr-x root root  4096 Nov 26 00:26 /usr/share/doc/epel-release-7
-	// -rw-r--r-- root root 18385 Nov 26 00:23 /usr/share/doc/epel-release-7/GPL
+	// -rw-r--r-- root root  1662 Nov 25 16:23 /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
+	// -rw-r--r-- root root  1056 Nov 25 16:23 /etc/yum.repos.d/epel-testing.repo
+	// -rw-r--r-- root root   957 Nov 25 16:23 /etc/yum.repos.d/epel.repo
+	// -rw-r--r-- root root    41 Nov 25 16:23 /usr/lib/rpm/macros.d/macros.epel
+	// -rw-r--r-- root root  2813 Nov 25 16:23 /usr/lib/systemd/system-preset/90-epel.preset
+	// -rwxr-xr-x root root  4096 Nov 25 16:26 /usr/share/doc/epel-release-7
+	// -rw-r--r-- root root 18385 Nov 25 16:23 /usr/share/doc/epel-release-7/GPL
 }
