71761aa30f9cc118b83402030880e8342e5d391f
Ryan Armstrong <ryan@cavaliercoder.com>
2017-06-18 02:43:18 +0800

    Applied gofmt -s

--- dependency_test.go
+++ dependency_test.go
@@ -12,22 +12,22 @@ type DepTest struct {
 
 func TestDependencies(t *testing.T) {
 	tests := []DepTest{
-		DepTest{NewDependency(DepFlagAny, "test", 0, "", ""), "test"},
-		DepTest{NewDependency(DepFlagAny, "test", 0, "1", ""), "test 1"},
-		DepTest{NewDependency(DepFlagAny, "test", 0, "1", "2"), "test 1.2"},
-		DepTest{NewDependency(DepFlagAny, "test", 1, "2", "3"), "test 2.3"},
-		DepTest{NewDependency(DepFlagLesser, "test", 0, "1", ""), "test < 1"},
-		DepTest{NewDependency(DepFlagLesser, "test", 0, "1", "2"), "test < 1.2"},
-		DepTest{NewDependency(DepFlagLesser, "test", 1, "2", "3"), "test < 2.3"},
-		DepTest{NewDependency(DepFlagLesserOrEqual, "test", 0, "1", ""), "test <= 1"},
-		DepTest{NewDependency(DepFlagLesserOrEqual, "test", 0, "1", "2"), "test <= 1.2"},
-		DepTest{NewDependency(DepFlagLesserOrEqual, "test", 1, "2", "3"), "test <= 2.3"},
-		DepTest{NewDependency(DepFlagGreaterOrEqual, "test", 0, "1", ""), "test >= 1"},
-		DepTest{NewDependency(DepFlagGreaterOrEqual, "test", 0, "1", "2"), "test >= 1.2"},
-		DepTest{NewDependency(DepFlagGreaterOrEqual, "test", 1, "2", "3"), "test >= 2.3"},
-		DepTest{NewDependency(DepFlagLesser, "test", 0, "1", ""), "test < 1"},
-		DepTest{NewDependency(DepFlagLesser, "test", 0, "1", "2"), "test < 1.2"},
-		DepTest{NewDependency(DepFlagLesser, "test", 1, "2", "3"), "test < 2.3"},
+		{NewDependency(DepFlagAny, "test", 0, "", ""), "test"},
+		{NewDependency(DepFlagAny, "test", 0, "1", ""), "test 1"},
+		{NewDependency(DepFlagAny, "test", 0, "1", "2"), "test 1.2"},
+		{NewDependency(DepFlagAny, "test", 1, "2", "3"), "test 2.3"},
+		{NewDependency(DepFlagLesser, "test", 0, "1", ""), "test < 1"},
+		{NewDependency(DepFlagLesser, "test", 0, "1", "2"), "test < 1.2"},
+		{NewDependency(DepFlagLesser, "test", 1, "2", "3"), "test < 2.3"},
+		{NewDependency(DepFlagLesserOrEqual, "test", 0, "1", ""), "test <= 1"},
+		{NewDependency(DepFlagLesserOrEqual, "test", 0, "1", "2"), "test <= 1.2"},
+		{NewDependency(DepFlagLesserOrEqual, "test", 1, "2", "3"), "test <= 2.3"},
+		{NewDependency(DepFlagGreaterOrEqual, "test", 0, "1", ""), "test >= 1"},
+		{NewDependency(DepFlagGreaterOrEqual, "test", 0, "1", "2"), "test >= 1.2"},
+		{NewDependency(DepFlagGreaterOrEqual, "test", 1, "2", "3"), "test >= 2.3"},
+		{NewDependency(DepFlagLesser, "test", 0, "1", ""), "test < 1"},
+		{NewDependency(DepFlagLesser, "test", 0, "1", "2"), "test < 1.2"},
+		{NewDependency(DepFlagLesser, "test", 1, "2", "3"), "test < 2.3"},
 	}
 
 	for i, test := range tests {
