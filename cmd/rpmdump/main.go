a5ec290002fb95455bededcd4358a93534b98d9e
Ryan Armstrong <ryan@cavaliercoder.com>
2017-06-18 01:38:58 +0800

    Added byte formatting to rpmdump

--- cmd/rpmdump/main.go
+++ cmd/rpmdump/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/cavaliercoder/go-rpm"
+	"math"
 	"os"
 	"strings"
+
+	"github.com/cavaliercoder/go-rpm"
 )
 
 func main() {
@@ -55,7 +57,7 @@ func raw(path string) {
 			case []string:
 				ss := ix.Value.([]string)
 				if len(ss) == 1 && strings.Index(ss[0], "\n") == -1 {
-					fmt.Printf("      value: [ \"%v\" ]\n", ss[0])
+					fmt.Printf("      value: [\"%v\"]\n", ss[0])
 				} else {
 					fmt.Printf("      value:\n")
 					for _, s := range ss {
@@ -68,6 +70,48 @@ func raw(path string) {
 								fmt.Printf("        %v\n", line)
 							}
 						}
+					}
+				}
+
+			case []byte:
+				b := ix.Value.([]byte)
+				if len(b) <= 16 {
+					fmt.Print("      value: [")
+					for i, x := range b {
+						if i > 0 {
+							fmt.Print(" ")
+						}
+						fmt.Printf("%02x", x)
+					}
+					fmt.Println("]")
+				} else {
+					fmt.Println("      value: |")
+					for i := 0; i < len(b); i += 16 {
+						fmt.Printf("        %08x  ", i)
+						l := int(math.Min(16, float64(len(b)-i)))
+						for j := 0; j < l; j++ {
+							fmt.Printf("%02x ", b[i+j])
+							if j == 7 {
+								fmt.Print(" ")
+							}
+						}
+
+						for j := 0; j < 16-l; j++ {
+							fmt.Print("   ")
+						}
+						if l < 8 {
+							fmt.Print(" ")
+						}
+
+						s := [16]byte{}
+						copy(s[:], b[i:])
+						for j := 0; j < 16; j++ {
+							// print '.' if char is not printable ascii
+							if s[j] < 32 || s[j] > 126 {
+								s[j] = 46
+							}
+						}
+						fmt.Printf(" |%s|\n", s)
 					}
 				}
 
